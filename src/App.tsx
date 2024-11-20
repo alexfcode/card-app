53cf2185d2ad415869e5acfe95fd8fa398100630
alexf <alexf@tut.by>
2024-11-20 19:52:45 +0300

    make

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      Card
+      <Box>Card</Box>
     </div>
   );
 }
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+  width: 300px;
+  height: 350px;
+  box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.1);
+  background: #fff;
+`;
 
 export default App;
